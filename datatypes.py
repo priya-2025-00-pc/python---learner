7cd531841cc988bdd0a186556a473263260872d2
priya-2025-00-pc <kanchipriyanka543@gmail.com>
2026-06-30 11:43:50 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+name="Priyanka"
+age=18
+cgpa=8.8
+marks=(10,9,10)
+subjects=["c","dsa","sql"]
+is_student=True
+complex=3+4j
+languages = {"Python", "Java", "C"}
+student = {
+    "Name": "Priyanka",
+    "Age": 18,
+    "cgpa": 8.8
+}
+print(name)
+print(age)
+print(cgpa)
+print(marks)
+print(subjects)
+print(is_student)
+print(complex)
+print(languages)
+print(student)
