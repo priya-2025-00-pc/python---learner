e6c32c1db162e7f619f6f42d6b6c66b6adff33e6
priya-2025-00-pc <kanchipriyanka543@gmail.com>
2026-07-03 15:17:23 +0530

    Create four largest.py

---
+++ if-else/four largest.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+largest = a
+
+if b > largest:
+    largest = b
+
+if c > largest:
+    largest = c
+
+if d > largest:
+    largest = d
+
+print(largest)
