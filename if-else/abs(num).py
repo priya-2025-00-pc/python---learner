13a8d3595cb7065a104a44398f3b27485a430a3e
priya-2025-00-pc <kanchipriyanka543@gmail.com>
2026-07-03 15:50:51 +0530

    Add check for three-digit positive number

---
+++ if-else/abs(num).py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+
+if 100 <= num <= 999:
+    print("The number is a three-digit positive number.")
+else:
+    print("The number is NOT a three-digit positive number.")
