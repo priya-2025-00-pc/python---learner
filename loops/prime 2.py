3cd3dec39483705ff6103a18df2a62f5c5886f0f
priya-2025-00-pc <kanchipriyanka543@gmail.com>
2026-07-04 14:28:36 +0530

    Create prime 2.py

---
+++ loops/prime 2.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+flag = 0
+if num <= 1:
+    flag = 1
+else:
+     for i in range(2, num):
+        if num % i == 0:
+            flag= 1
+            break
+if flag == 0:
+    print("Prime")
+else:
+    print("Not Prime")
